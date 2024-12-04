02fb7629f75646a3589426beac7f16ce5fff69e8
xogns <xogns40430@naver.com>
2024-12-04 21:57:15 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1544 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.cc
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int cnt[10001] = { 0 };
+	int n,count;
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> count;
+		cnt[count]++;
+	}
+
+	for (int i = 1; i < 10001; i++)
+		for (int j = 0; j < cnt[i]; j++)
+			cout << i<<'\n';
+
+	return 0;
+}
