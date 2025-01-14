cdc883acd6cc1b909ae4a904a26b12e1cf2ea25d
xogns <xogns40430@naver.com>
2025-01-14 21:34:07 +0900

    [Bronze V] Title: 등록, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+#define nl '\n'
+
+using namespace std;
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cout << 121 << nl << "rod12" << nl;
+
+    return 0;
+}
