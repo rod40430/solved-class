7a6f17545ace0714e3c653b14ad816cf09de2db8
xogns <xogns40430@naver.com>
2025-01-14 21:38:21 +0900

    [Bronze V] Title: 오늘 날짜, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+#define nl '\n'
+
+using namespace std;
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cout << 2025 << '-' << "01" << '-' << 14 << nl;
+
+    return 0;
+}
