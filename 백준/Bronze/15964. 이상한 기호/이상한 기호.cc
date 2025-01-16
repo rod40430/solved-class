9b8350540c455755e754794a7e6f850638fded02
xogns <xogns40430@naver.com>
2025-01-17 00:02:22 +0900

    [Bronze V] Title: 이상한 기호, Time: 0 ms, Memory: 2020 KB, Score: 30 point -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+#define nl '\n'
+
+using namespace std;
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int a, b;
+	cin >> a >> b;
+
+	cout << (a + b) * (a - b) << nl;
+
+    return 0;
+}
